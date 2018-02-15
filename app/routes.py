8b6ae7751fb1fe9ef9d41c45bca5ff4359a5af1d
hawkdawg <johnjhawk@gmail.com>
2018-02-15 10:02:39 -0800

    login form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -10,10 +11,12 @@ def index():
     questions = [
             {
                 'author': {'username': 'Hank'},
+                'title': 'T ball q 1',
                 'body': 'What is a tennis ball?'
             },
             {
                 'author': {'username': 'Dan'},
+                'title': 'T ball q 2',
                 'body': 'Why are there tennis balls?'
             }
     ]
@@ -27,4 +30,18 @@ def index():
                 'body': 'To chase.'
             }
     ]
-    return render_template('index.html', title='Home', user=user, questions=questions, answers=answers)
+    return render_template(
+            'index.html',
+            title='Home',
+            user=user,
+            questions=questions,
+            answers=answers)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template(
+            'login.html',
+            title='Sign In',
+            form=form)
