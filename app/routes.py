f6383ea9fbd97465bf5678d471fde8723e7b1c7d
hawkdawg <johnjhawk@gmail.com>
2018-02-15 05:40:37 -0800

    fake some data

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,30 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "FAQandA"
+    user = {'username': 'Beanie'}
+
+    questions = [
+            {
+                'author': {'username': 'Hank'},
+                'body': 'What is a tennis ball?'
+            },
+            {
+                'author': {'username': 'Dan'},
+                'body': 'Why are there tennis balls?'
+            }
+    ]
+    answers = [
+            {
+                'author': {'username': 'Beanie'},
+                'body': 'You chase tennis balls.'
+            },
+            {
+                'author': {'username': 'Beanie'},
+                'body': 'To chase.'
+            }
+    ]
+    return render_template('index.html', title='Home', user=user, questions=questions, answers=answers)
