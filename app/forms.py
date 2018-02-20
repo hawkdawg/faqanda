bc941a532af045444d5a529981c7018a2ef782d2
hawkdawg <johnjhawk@gmail.com>
2018-02-20 06:12:31 -0800

    overhaul

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -12,3 +13,35 @@ class LoginForm(FlaskForm):
             validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField(label='Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(
+            label='Username',
+            validators=[DataRequired()])
+    email = StringField(
+            label='Email',
+            validators=[DataRequired(), Email()])
+    password = PasswordField(
+            label='Password',
+            validators=[DataRequired()])
+    password2 = PasswordField(
+            label='Repeat Password',
+            validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField(label='Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            emanresu = username.data[::-1]
+            if User.query.filter_by(username=emanresu).first() is None:
+                raise ValidationError(
+                        'Please use a different username. Perhaps: "{}"?'.format(
+                                emanresu))
+            else:
+                raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
