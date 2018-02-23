22d608571072f628d4346eacc2469bb94937c498
hawkdawg <johnjhawk@gmail.com>
2018-02-23 06:20:00 -0800

    logging support

--- config.py
+++ config.py
@@ -8,3 +8,12 @@ class Config:
             'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    EMAIL_FAILURES = int(os.environ.get('EMAIL_FAILURES') or 0)
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_ADMINS = os.environ.get('MAIL_ADMINS') or ['your-email@example.com']
+
+    LOG_FAILURES = int(os.environ.get('LOG_FAILURES') or 0)
